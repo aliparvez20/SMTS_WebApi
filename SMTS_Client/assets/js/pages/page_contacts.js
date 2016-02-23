767bf91c66ebcd2a6b29459956b1f5befb243180
parvez.ali@indpro.se <parvez.ali@indpro.se>
2016-02-23 18:36:11 +0530

    Fixed Map
    Fixed Map

--- SMTS_Client/assets/js/pages/page_contacts.js
+++ SMTS_Client/assets/js/pages/page_contacts.js
@@ -1,37 +1,37 @@
 var ContactPage = function () {
 
     return {
-        
-    	//Basic Map
+
+        //Basic Map
         initMap: function () {
-			var map;
-			$(document).ready(function(){
-			  map = new GMaps({
-				div: '#map',
-				scrollwheel: false,				
-				lat: 40.748866,
-				lng: -73.988366
-			  });
-			  
-			  var marker = map.addMarker({
-				lat: 40.748866,
-				lng: -73.988366,
-	            title: 'Company, Inc.'
-		       });
-			});
+            var map;
+            $(document).ready(function () {
+                map = new GMaps({
+                    div: '#map',
+                    scrollwheel: false,
+                    lat: 22.556909,
+                    lng: 88.357357
+                });
+
+                var marker = map.addMarker({
+                    lat: 22.556909,
+                    lng: 88.357357,
+                    title: 'XXXXX'
+                });
+            });
         },
 
         //Panorama Map
         initPanorama: function () {
-		    var panorama;
-		    $(document).ready(function(){
-		      panorama = GMaps.createPanorama({
-		        el: '#panorama',
-		        lat : 40.748866,
-		        lng : -73.988366
-		      });
-		    });
-		}        
+            var panorama;
+            $(document).ready(function () {
+                panorama = GMaps.createPanorama({
+                    el: '#panorama',
+                    lat: 22.556909,
+                    lng: 88.357357
+                });
+            });
+        }
 
     };
 }();
